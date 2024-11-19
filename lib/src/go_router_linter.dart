ed325da0586ca44ed7113773b3bfd7a8a034058f
Dongmin Yu <ydm2790@gmail.com>
2024-11-19 21:59:29 +0900

    chore: 🎉 first commit

---
+++ lib/src/go_router_linter.dart
@@ -0,0 +1,7 @@
+/// {@template go_router_linter}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class GoRouterLinter {
+  /// {@macro go_router_linter}
+  const GoRouterLinter();
+}
---
+++ test/src/go_router_linter_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:go_router_linter/go_router_linter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('GoRouterLinter', () {
+    test('can be instantiated', () {
+      expect(GoRouterLinter(), isNotNull);
+    });
+  });
+}
