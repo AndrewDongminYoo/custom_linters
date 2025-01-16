961603a0a4bdf2e5a893a9f36c689d4bd7ca8152
DongminYu <ydm2790@gmail.com>
2025-01-16 12:52:01 +0900

    fix: 🤡 modify `State` class to look like a flutter widget (change to abstract class)

--- packages/flutter_best_practices_lints/example/lib/fake_state_class.dart
+++ packages/flutter_best_practices_lints/example/lib/fake_state_class.dart
@@ -1,8 +1,19 @@
-// expect_lint: matching_class_and_file_name
-class State {}
+abstract class State<T> {
+  /// The current configuration.
+  ///
+  /// A [State] object's configuration is the corresponding `StatefulWidget`
+  /// instance. This property is initialized by the framework before calling
+  /// `initState`. If the parent updates this location in the tree to a new
+  /// widget with the same `runtimeType` and `Widget.key` as the current
+  /// configuration, the framework will update this property to refer to the new
+  /// widget and then call `didUpdateWidget`, passing the old configuration as
+  /// an argument.
+  T get widget => _widget!;
+  T? _widget;
+}
 
 // expect_lint: single_class_per_file
 class FakeStateClass {}
 
 // expect_lint: single_class_per_file, matching_class_and_file_name
-class FakeInheritedClass extends State {}
+class FakeInheritedState extends State {}
